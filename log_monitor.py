536d4e28ea1c29d2a25943fbbb17861bd7eca05a
MatthewDaffern <38901216+MatthewDaffern@users.noreply.github.com>
2018-11-05 12:00:42 -0800

    last commit for a while, I cannot use this for what I want

--- log_monitor.py
+++ log_monitor.py
@@ -1,20 +1,19 @@
 import pathlib
-from subprocess import run,Popen
-from os import mkdir
+from subprocess import run
+import os
 from os.path import isfile, join
 from shutil import copy2 as cp
 import time
 
 
-
-
 def config_initializer():
     # path where the outlook files are saved.
     path_var = 'C:\\'
-    config_file = path_var
+    config_file = open(path_var, 'r')
     config = config_file.readlines()
     config_file.close()
     return config
+
 
 def path_initializer(config_path_input):
     path_input = config_path_input.split('\\')
@@ -22,12 +21,10 @@ def path_initializer(config_path_input):
 
 
 def delete_file(file_path):
-    subprocess.run(['del',file_path])
+    run(['del', file_path])
     result = 'deleted ' + file_path
     print(result)
     return result
-
-
 
 
 def file_reader(file_path):
@@ -35,16 +32,18 @@ def file_reader(file_path):
     file_lines = file.readlines()
     return file_lines
 
-def alert_wrapper(str_input, alert_list, alert_function):
-    alerts = alert_list
+
+def alert_wrapper(str_input, alert_list_input, alert_function_input):
+    alerts = alert_list_input
     for i in str_input.split(' '):
         if i in alerts:
-            alert_function(i)
+            alert_function_input(i)
+
 
 def list_of_files(path_input):
     raw_list = os.path(path_input)
     list_of_files_final = list()
-    for i in list_of_files_final:
+    for i in raw_list:
         if isfile(i):
             list_of_files_final.append(i)
     return list_of_files_final
@@ -55,30 +54,42 @@ def alert_list():
     return alerts
 # Needs alerts to go in the tuple
 
-def file_copier(old_path,new_path):
+
+def file_copier(old_path, new_path):
     cp(old_path, new_path)
     return 'copied the files!'
 
-def the_loop()
+
+def the_loop():
     while True:
         the_whole_daemon_func()
         time.sleep(7200)
 
 
+def alert_function(str_input):
+    actual_time = time.localtime(time.time())
+    alert = str_input + str(actual_time)
+    smtp_func(alert)
+    return alert
+
+
 def the_whole_daemon_func():
     the_path = path_initializer(config_initializer())
     file_list = list_of_files(the_path)
-    finished_files = list_of_files()# the final path
-    delete_file_list = set()
+    finished_files = list_of_files()  # the final path
     for i in file_list:
         if i not in finished_files:
             file_lines = file_reader(i)
             file_copier(i, finished_files)
-            for i in file_lines:
+            for item in file_lines:
                 alert_wrapper(i, alert_list(), alert_function)
-    done_message = 'finished processing @ '+ time.localtime(time.time())
+    done_message = 'finished processing @ ' + time.localtime(time.time())
     log_file = open('file.log', 'r+')
     log_file.write(done_message)
     print(done_message)
     log_file.close()
 
+
+# smtp_func()
+# Was going to make this, but PHI requirements and my NDA prevents any
+# customer data leaving the LAN without approval
